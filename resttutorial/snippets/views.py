afe2ed41be6e3f99fd9f5386bce3d12df8d57503
Igor <i.rosa@bn.org.pl>
2019-10-30 14:27:02 +0100

    Usunąłem zbędne komentarze i importy refactoring

--- resttutorial/snippets/views.py
+++ resttutorial/snippets/views.py
@@ -1,14 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse, Http404
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from django.http import Http404
 from rest_framework.views import APIView
-
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework.decorators import api_view
 from rest_framework import status, mixins, generics
-from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.response import Response
 
 
