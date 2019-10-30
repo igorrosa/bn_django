242c473af3827582b8621a4b239ece697bd8bd3a
Igor <i.rosa@bn.org.pl>
2019-10-30 14:24:48 +0100

    W widoku klasowym SnippetList - użyłem mixinów i generycznego widoku znacznie zmniejszyła się objętość kodu

--- resttutorial/snippets/views.py
+++ resttutorial/snippets/views.py
@@ -7,70 +7,22 @@ from rest_framework.views import APIView
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework.decorators import api_view
-from rest_framework import status
+from rest_framework import status, mixins, generics
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.response import Response
 
 
-# Create your views here.
-# GET /snippets - lista snippetów
-# POST /snippets - tworzy
-# @api_view(['GET', 'POST']) # gdy było csrf exempt nie trzeba tokena
-# def snippet_list(request, format=None):
-#     if request.method == "GET":
-#         snippets = Snippet.objects.all()
-#         serializer = SnippetSerializer(snippets, many=True)
-#         return Response(serializer.data)
-#     elif request.method == "POST":
-#         data = JSONParser().parse(request)
-#         serializer = SnippetSerializer(data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)#statusy ściagniete z rest framework
-#         return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST) #JSONresponse na response z rest frameworks
+class SnippetList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    def get(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
 
-class SnippetList(APIView):
-    '''
-    List all snippets,
-    create a new snippet
-    '''
+    def post(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
 
-    def get(self, request, format=None):
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = SnippetSerializer(snippets, many=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
-
-
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def snippet_detail(request, pk, format=None):
-#     '''
-#     Get snippet with id, update, delete
-#     '''
-#     try:
-#         snippet = Snippet.objects.get(pk=pk)
-#     except Snippet.DoesNotExist:
-#         return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-#
-#     if request.method == "GET":
-#         serializer = SnippetSerializer(snippet)
-#         return Response(serializer.data)
-#     if request.method == "PUT":
-#         data = JSONParser().parse(snippet)
-#         serializer = SnippetSerializer(snippet, data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#     elif request.method == "DELETE":
-#         snippet.delete()
-#         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
 
 class SnippetDetail(APIView):
     '''
